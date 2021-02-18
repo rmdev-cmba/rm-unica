2c2ae1f43866cd920a9fcacf019520f853baa374
Regis Oliveira <regis.aoc@gmail.com>
2021-02-17 23:01:50 -0300

    test getStaticProps

---
+++ pages/tempo.js
@@ -0,0 +1,26 @@
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return(
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
+        </div>
+    )
+}
+
+// Nada dentro do "getStaticProps()" vai para o Frontend
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+
+}
+
+export default Tempo;
