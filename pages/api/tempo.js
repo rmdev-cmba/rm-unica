21ccec5dfed43dbeb146b8a653e667afba23b172
Regis Oliveira <regis.aoc@gmail.com>
2021-02-18 23:43:38 -0300

    adicao cache

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -7,6 +7,7 @@ async function tempo(request, response) {
 
     const retorno = await responseBling.json();
 
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
     
 
     response.json({
